9003ff7322149a0479447d75551f493f8d01c7d0
ebonini <44753902+ebonini@users.noreply.github.com>
2024-12-02 21:08:04 -0300

    Update extrair.py

--- extrair.py
+++ extrair.py
@@ -11,7 +11,7 @@ def extrair_nomes_m3u(arquivo_m3u):
         return []
 
     nomes = []
-    for linha em linhas:
+    for linha in linhas:
         if linha.startswith('#EXTINF:'):
             info = linha.split(',')
             nome = info[-1].strip()
